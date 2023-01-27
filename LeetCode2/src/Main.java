7d7b40e9a6f44577603997454c0779ee72cbc1d9
strorikaster <strorikaster@gmail.com>
2023-01-27 16:30:37 +0300

    Add files via upload

--- LeetCode2/src/Main.java
+++ LeetCode2/src/Main.java
@@ -1,8 +1,12 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -25,20 +29,21 @@ public class Main {
         Object o = new Object();
 
 
-        list1.add(null);//<? extends A> producer пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ null
-        //list1.add(d);<? extends A> producer пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ null
+        list1.add(null);//<? extends A> producer nothing add allow exept null
+        //list1.add(d);<? extends A> producer nothing add allow exept null
         list2.add(b);
         list2.add(c);
         //end generic test
+
 
 
         //System.out.println(removeElement(nums, 3));
         //System.out.println(searchInsert(nums, 5));
         //System.out.println(plusOne(digits).toString());
         // System.out.println(getDuplicatedWordsInString("Java i like this programming language java"));
-        String str = "РРІР°РЅРѕРІ РРІР°РЅ РРІР°РЅРѕРІРёС‡";
+        String str = "Иванов Иван Иванович";
         //System.out.println(reverseString(str));
-        //System.out.println(exchangeSymbols(str,  'пїЅ', 'пїЅ'));
+        //System.out.println(exchangeSymbols(str,  'а', 'т'));
         //System.out.println(stringToInt("-23"));
         //outputNumbers();
         //System.out.println(stringToDouble("23"));
@@ -46,10 +51,9 @@ public class Main {
 //        int[] nums2 = {2,3,4,6,8};
 //        printNonRepeatElementsInFirstArray(nums1, nums2);
         //System.out.println(reverseString2(str));
-        System.out.println(elm("MQNAAAAzzzBBCCCvvvvvvDDDDEBBBBBBBBBBvvv"));
 
-        int[] nums3 = {1, 2, 3, 4, 3, 5};
-        // System.out.println(containsDubl(nums3));
+        int[] nums3 = {1,2,3,4,3,5};
+       // System.out.println(containsDubl(nums3));
 
         //System.out.println(findFirstNonRepeatLetter("aaaabccccdggg"));
         String[] strArr = {"sdffgg", "fgdfg", "aaaahjfhj", "dfdfd", "aaa", "tyttyty"};
@@ -58,24 +62,33 @@ public class Main {
         //System.out.println(Arrays.toString(sort(sourceArray)));
         double[] sourceDoubleArray = {/*2.7,6.0,4.5,8.3,9.4,5.9,7.0*/0.0};
 //        System.out.println(foo(sourceDoubleArray));
+        String str5 = "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBB";
+        System.out.println(rle(str5));
+
+
+
         Person person1 = new Person("Alex", "Zotov", 41);
-        Person person2 = new Person();
+        Person person2 =  new Person();
         person2.setAge(41);
         person2.setLastName("Zotov");
         person2.setFirstName("Alex");
 
-        //System.out.println(person1.equals(person2));
+       // System.out.println(person1.equals(person2));
 
-        int[] nums1 = {2, 9, 3, 8, 4, 7, 5, 6, 1};
-        int[] nums2 = {6, 5, 4, 9, 8, 1, 0, 3, 4};
-//        int[] nums1 = {1,2,3,5,7};
-//        int[] nums2 = {2,3,4,6,8};
-        //System.out.println(Arrays.toString(repeatedValueArray(nums1, nums2)));
+        String s = "anagram";
+        String t = "nagaram";
+//        System.out.println(isAnagram(s, t));
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
 
-        int[] arr1 = {1,3,4,6,78,23,56,78};
-        int[] arr2 = {1,2,5,6,67,89,90};
+        int[] arr1 = {2,4,7,9,11,15};
+        int[] arr2 = {1,3,5,7,8,9,12,16};
+
+        System.out.println(Arrays.toString(sortedArray(arr1, arr2)));
         System.out.println(Arrays.toString(mergeSortedArraysWithStreamApi(arr1, arr2)));
+
     }
+
 
     public static int removeElement(int[] nums, int val) {
         int count = 0;
@@ -110,7 +123,7 @@ public class Main {
         return lo;
     }
 
-    //пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+    //сам решил
     public int lengthOfLastWord(String s) {
         String[] strMas = s.split(" ");
         return (strMas[strMas.length - 1]).length();
@@ -212,7 +225,7 @@ public class Main {
         }
 
         for (int i = 0; i < str.length(); i++) {
-            result = result * 10 + ((str.charAt(i))) - '0';
+                result = result * 10 + ((str.charAt(i))) - '0';
         }
         return result * isNegative;
     }
@@ -233,17 +246,10 @@ public class Main {
 
 
     //Generics and PECS
-    public static class D {
-    }
-
-    public static class A extends D {
-    }
-
-    public static class B extends A {
-    }
-
-    public static class C extends B {
-    }
+    public static class D{}
+    public static class A extends D{}
+    public static class B extends A{}
+    public static class C extends B{}
 
     public static Double stringToDouble(String str) {
         Double result = 0.0;
@@ -255,68 +261,69 @@ public class Main {
         }
         for (int i = 0; i < str.length(); i++) {
 
-            if (str.charAt(i) == ',') {
-                ++i;
-                pow = str.length() - i;
-            }
+                if(str.charAt(i) == ',') {
+                    ++i;
+                    pow = str.length() - i;
+                }
             result = result * 10 + ((str.charAt(i))) - '0';
-        }
-        result = isNegative * result / Math.pow(10, pow);
+            }
+        result = isNegative*result/Math.pow(10, pow);
         return result;
     }
 
     public static void printNonRepeatElementsInFirstArray(int[] nums1, int[] nums2) {
-        int k = 0;
-        int l = 0;
+       int k = 0;
+       int l = 0;
 
-        while (k < nums1.length) {
-            if (nums1[k] != nums2[l]) {
-                System.out.println(nums1[k]);
-            } else {
-                l++;
-            }
-            k++;
-        }
+       while (k < nums1.length) {
+           if(nums1[k] != nums2[l]) {
+               System.out.println(nums1[k]);
+           } else {
+               l++;
+           }
+           k++;
+       }
     }
 
-    public static boolean containsDubl(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        public static boolean containsDubl(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], 1);
-            } else if (map.containsKey(nums[i])) {
-                map.computeIfPresent(nums[i], (key, val) -> val + 1);
+            for (int i = 0; i < nums.length; i++) {
+                if(!map.containsKey(nums[i])) {
+                    map.put(nums[i], 1);
+                } else if(map.containsKey(nums[i])) {
+                    map.computeIfPresent(nums[i], (key, val) -> val + 1);
 //
+                }
             }
+            return map
+                   .entrySet()
+                   .stream()
+                   .anyMatch(entry -> entry.getValue() > 1);
+
+
+
         }
-        return map
-                .entrySet()
-                .stream()
-                .anyMatch(entry -> entry.getValue() > 1);
 
+        public static Character findFirstNonRepeatLetter(String str) {
+         Map<Character, Integer> map = new HashMap<>();
 
-    }
-
-    public static Character findFirstNonRepeatLetter(String str) {
-        Map<Character, Integer> map = new HashMap<>();
-
-        char[] charArr = str.toCharArray();
-        for (int i = 0; i < str.length(); i++) {
-            if (!map.containsKey(charArr[i])) {
-                map.put(charArr[i], 1);
-            } else if (map.containsKey(charArr[i])) {
-                map.computeIfPresent(charArr[i], (key, val) -> val + 1);
+            char[] charArr = str.toCharArray();
+            for (int i = 0; i < str.length(); i++) {
+                if(!map.containsKey(charArr[i])) {
+                    map.put(charArr[i], 1);
+                } else if(map.containsKey(charArr[i])) {
+                    map.computeIfPresent(charArr[i], (key, val) -> val + 1);
+                }
             }
+            //System.out.println(map.toString());
+            return map
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue().equals(1))
+                    .map(Map.Entry::getKey)
+                    .findFirst().get();
         }
-        //System.out.println(map.toString());
-        return map
-                .entrySet()
-                .stream()
-                .filter(entry -> entry.getValue().equals(1))
-                .map(Map.Entry::getKey)
-                .findFirst().get();
-    }
 
 //        public static String reverseString2(String str) {
 //            String str1 = str.toLowerCase();
@@ -334,15 +341,14 @@ public class Main {
 
     //Make Method filter strings "aaa", method get array of strings
     public static void fiterTripleA(String[] stringCollection) {
-        /*String[] filetredStringArray  =*/
-        Arrays.stream(stringCollection).filter(s -> !s.startsWith("aaa")).forEach(s -> System.out.println(s));
+        /*String[] filetredStringArray  =*/ Arrays.stream(stringCollection).filter(s -> !s.startsWith("aaa")).forEach(s -> System.out.println(s));
     }
 
     //Make bubble sort method with exception, if incoming array is empty
     public static int[] sort(int[] sourceArray) throws MyException {
-        if (sourceArray.length == 0) throw new MyException("The length of sourceArray not be equals 0");
+      if(sourceArray.length == 0) throw new MyException("The length of sourceArray not be equals 0");
         //int temp = 0;
-        for (int i = 0; i < sourceArray.length; i++) {
+        for (int i = 0; i < sourceArray.length ; i++) {
 
             for (int j = 0; j < sourceArray.length - 1; j++) {
 
@@ -354,15 +360,15 @@ public class Main {
             }
         }
         return sourceArray;
-    }
+        }
 
-    public static double foo(double[] sourceDoubleArray) throws MyDoubleException {
-        Arrays.sort(sourceDoubleArray);
-        double sum = 0.0;
-        Double result = 0.0;
+        public static double foo(double[] sourceDoubleArray) throws MyDoubleException {
+            Arrays.sort(sourceDoubleArray);
+            double sum = 0.0;
+            Double result = 0.0;
 
 
-        try {
+            try {
             double min = sourceDoubleArray[0];
             double max = sourceDoubleArray[sourceDoubleArray.length - 1];
 
@@ -384,56 +390,106 @@ public class Main {
             }
         }
 
-        return result;
-    }
+            return result;
+        }
 
-    public static int[] repeatedValueArray(int[] nums1, int[] nums2) {
-        int[] nums3 = new int[nums1.length];
-        int k = 0;
+        public static boolean isAnagram(String s, String t) {
+            boolean result = false;
+            char[] sArray = s.toCharArray();
+            char[] tArray = t.toCharArray();
+//            System.out.println(Arrays.toString(sArray));
+//            System.out.println(Arrays.toString(tArray));
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    //System.out.println(nums1[i]);
-                    nums3[k] = nums1[i];
-                    k++;
-                    break;
+            Arrays.sort(sArray);
+            Arrays.sort(tArray);
+
+//            System.out.println(Arrays.toString(sArray));
+//            System.out.println(Arrays.toString(tArray));
+
+            if(Arrays.equals(sArray, tArray)) {
+                result = true;
+            }
+            return result;
+        }
+
+        public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+            for (int i = 0; i < strs.length; i++) {
+                char[] fArr = strs[i].toCharArray();
+                Arrays.sort(fArr);
+                List<String> list = new ArrayList<>();
+                for (int j = i + 1; j < strs.length - 1; j++) {
+
+                    char[] sArr = strs[j].toCharArray();
+
+                    Arrays.sort(sArr);
+                    if(Arrays.equals(fArr, sArr)) {
+                        list.add(strs[i]);
+                        list.add(strs[j]);
+                    } else if (!Arrays.equals(fArr, sArr)){
+                        list.add(strs[i]);
+                    }
                 }
+                res.add(list);
             }
+            return res;
         }
-        int[] nums4 = new int[k];
-        System.arraycopy(nums3, 0, nums4, 0, k);
-        return nums4;
-    }
 
-    public static String elm(String str) {
-        int count = 1;
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < str.length() - 1; i++) {
-            char ch;
-            if (str.charAt(i) == str.charAt(i + 1)) {
-                count++;
-            } else {
-                ch = str.charAt(i);
-                sb.append(ch).append(count);
-                count = 1;
+        public static int[] sortedArray (int[] arr1, int[] arr2) {
+            int[] mergedSortedArray = new int[arr1.length + arr2.length];
+            int i = 0;
+            int j = 0;
+            int r = 0;
+            //for (int i = 0; i < (arr1.length < arr2.length?arr1.length:arr2.length); i++) {
+            while(i < arr1.length && j < arr2.length) {
+                if (arr1[i] < arr2[j]) {
+                    mergedSortedArray[r] = arr1[i];
+                    i++;
+                } else {
+                    mergedSortedArray[r] = arr2[j];
+                    j++;
+                }
+                r++;
             }
-
-            if(i + 1 == str.length() - 1) {
-                ch = str.charAt(i + 1);
-                sb.append(ch).append(count);
+                if (i < arr1.length) {
+                    System.arraycopy(arr2, i, mergedSortedArray, r, arr1.length - i);
+                }
+                if (j < arr2.length) {
+                    System.arraycopy(arr2, j, mergedSortedArray, r, arr2.length - j);
+                }
+                return mergedSortedArray;
             }
-        }
-        return sb.toString();
-    }
 
     public static int[] mergeSortedArraysWithStreamApi(int[] arr1, int[] arr2) {
-       return IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2)).sorted().toArray();
+        return IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2)).sorted().toArray();
     }
-}
 
+
+        public static String rle(String input) {
+            int count = 1;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.length() - 1; i++) {
+                char ch;
+                if (input.charAt(i) == input.charAt(i + 1)) {
+                    count++;
+                } else {
+                    ch = input.charAt(i);
+                    sb.append(ch).append(count);
+                    count = 1;
+                }
+
+                if(i + 1 == input.length() - 1) {
+                    ch = input.charAt(i + 1);
+                    sb.append(ch).append(count);
+                }
+            }
+            return sb.toString();
+        }
+
+
+}
 
 //bubble sort method exception
 class MyException extends Exception {
@@ -499,6 +555,37 @@ class Person {
     public int hashCode() {
         return 31*age*firstName.length()*lastName.length();
     }
+}
+
+record Order (Long id, List<Long> productIds, LocalDate orderDate) {
+
+    private static final int BIG_ORDER_SIZE = 10;
+
+    /*1*/    public Order {
+        if (productIds().isEmpty()) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    /*2*/    public Order (Long id) {
+        this(id, List.of(), LocalDate.now());
+    }
+
+    /*3*/    public static Order emptyOrder(Long id) {
+        return new Order(id);
+    }
+
+    /*4*/    public List<Long> productIds() {
+        return List.copyOf(productIds);
+    }
+
+    /*5    public void updateDate() {
+        this.orderDate = LocalDate.now();
+    }*/
+
+    /*6    public static boolean isBigOrder() {
+        return productIds.size() >= BIG_ORDER_SIZE;
+    }*/
 }
 
 
